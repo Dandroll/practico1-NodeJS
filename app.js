8c330e70ffe0e02bcab1fe23a62fa2ef99b73073
Dandroll <daniel.osvaldo.truyol@gmail.com>
2020-05-20 23:21:31 -0300

    practico1

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ fs.readdir('./', (err, files) => {
         console.log('\n\nArchivos ordenados a la inversa: ', files);
 
         let letra = 'A';
+        let cont = 0;
         letra = letra.toUpperCase();
         
         console.log('\n\nArchivos que empiezan con la letra ', letra, ':\n\n');
     
         files.forEach(file => { 
             if(file.charAt(0).toUpperCase() === letra){
+                cont++;
                 console.log('-', file, '\n');
             }
         }) 
+        console.log('Cantidad de archivos que empiezan con la letra ', letra, ': ', cont)
     }
 })
